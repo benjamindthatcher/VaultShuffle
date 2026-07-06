a0405548a2787a6003cd49c05ce6d2a27c4dad6e
Ben Thatcher <benjamindthatcher@gmail.com>
2026-07-07 00:51:34 +0100

    css cleanup

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,10 +3,9 @@ import { Dashboard } from "@/components/Dashboard";
 export default async function AppPage() {
   return (
     <>
-	<link rel="stylesheet" href="/styles.css" />
-	<link rel="stylesheet" href="/app-redesign.css" />
-	<link rel="stylesheet" href="/app-redesign-wishlist-collections.css" />
-	<Dashboard />
+      <link rel="stylesheet" href="/styles.css" />
+      <link rel="stylesheet" href="/app-redesign-wishlist-collections.css" />
+      <Dashboard />
     </>
   );
 }
